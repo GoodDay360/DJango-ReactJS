8bf6cd3f3638e0cfd0756564b86c860f74c9a011
unknown <istartgame31@gmail.com>
2024-02-23 16:37:32 +0700

    changed

--- worker/urls.py
+++ worker/urls.py
@@ -1,16 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include, re_path
-from worker.jobs import (borrow_book, join_library, statistic)
-
 
 
 urlpatterns = [
-    path('borrow_book/delete_outdated/', borrow_book.delete_outdated),
-    path('borrow_book/notify_return_book/', borrow_book.notify_return_book),
-    
-    path('join_library/delete_outdated/', join_library.delete_outdated),
-    
-    path('statistic/top_join_library/', statistic.top_join_library),
-    path('statistic/monthly_join_library/', statistic.monthly_join_library),
+
 ]
